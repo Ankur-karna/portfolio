0cc2cc9d7b90a5e87709f643a7416b53242a1f05
Ankur Karna <32448296+Ankur-karna@users.noreply.github.com>
2024-09-15 13:29:54 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,4 @@
+// Add any interactive features, such as dynamic loading of projects or social links
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Portfolio Loaded");
+});
